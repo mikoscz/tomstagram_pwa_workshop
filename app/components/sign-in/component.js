d34d36d2fd647a916c720d1e56c266651884521c
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2019-03-28 14:38:47 +0100

    Add sign in form

---
+++ app/components/sign-in/component.js
@@ -0,0 +1,16 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  session: service(),
+  router: service(),
+
+  actions: {
+    signIn(e) {
+      e.preventDefault();
+
+      this.session.authenticate(this.username);
+      this.router.transitionTo('index');
+    }
+  }
+});
