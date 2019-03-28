2f459415e2c43b5faf6a3d1599d5b3d393a498f8
Dawid Pośliński <dposlinski@gmail.com>
2019-03-28 21:43:08 +0100

    Make tests green again

--- tests/integration/components/footer/component-test.js
+++ tests/integration/components/footer/component-test.js
@@ -7,20 +7,8 @@ module('Integration | Component | footer', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{footer}}`);
 
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#footer}}
-        template block text
-      {{/footer}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
--- tests/integration/components/header/component-test.js
+++ tests/integration/components/header/component-test.js
@@ -7,20 +7,8 @@ module('Integration | Component | header', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{header}}`);
 
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#header}}
-        template block text
-      {{/header}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
