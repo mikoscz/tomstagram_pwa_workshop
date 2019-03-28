a9a29ade95207774e94ac926eaf5a109c769fa5c
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2019-03-28 20:36:13 +0100

    Add non blocking route rendering

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,20 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import Restricted from 'tomstagram/mixins/restricted';
+import ObjectProxy from '@ember/object/proxy';
+import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
 
 export default Route.extend(Restricted, {
   posts: service(),
 
   model() {
-    return this.posts.findAll()
+    const ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
+    const proxy = ObjectPromiseProxy.create({
+      promise: this.posts.findAll()
+    });
+
+    return {
+      posts: proxy
+    }
   }
 });
